495d03cc425760bfa122b1bd27bf5fd5b2f237e1
MDeBock <102190577+MDeBock@users.noreply.github.com>
2026-04-09 15:59:15 -0300

    feat: implement dashboard view and base bootstrap templates

--- taller/views.py
+++ taller/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Servicio
 
-# Create your views here.
+def dashboard(request):
+    # Traemos todos los servicios ordenados por los más recientes primero
+    servicios = Servicio.objects.all().order_by('-creado_el')
+    
+    contexto = {
+        'servicios': servicios
+    }
+    return render(request, 'taller/dashboard.html', contexto)
